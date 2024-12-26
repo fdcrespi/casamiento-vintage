d5483d0744cfb415fc415801e515e95c36e192e1
fdcrespi <fdcrespi@gmail.com>
2024-12-26 20:47:04 -0300

    Se elimina la carga de lista de invitados en main.js

--- script/main.js
+++ script/main.js
@@ -3,13 +3,13 @@
  document.addEventListener("DOMContentLoaded", function(event) {
  
   // Usar JavaScript para cargar los datos desde el archivo PHP
-  fetch('controller/lista_invitados.php')
+  /* fetch('controller/lista_invitados.php')
   .then(response => response.text())
   .then(data => {
       document.getElementById('personas').innerHTML = data;
   })
   .catch(error => {
       console.error('Error:', error);
-  });
+  }); */
 
 })
